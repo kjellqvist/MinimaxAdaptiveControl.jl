37cee11c88718ff76c84705ddf2014246eb8a10c
Olle Kjellqvist <olle.kjellqvist@control.lth.se>
2021-02-11 22:26:45 +0100

    removed explicit include

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,3 @@
-include("../src/MinimaxAdaptiveControl.jl")
 using MinimaxAdaptiveControl, Documenter
 makedocs(sitename="Minimax Adaptive Control", modules=[MinimaxAdaptiveControl])
 deploydocs(repo = "github.com/kjellqvist/MinimaxAdaptiveControl.jl.git")
