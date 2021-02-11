65f777ad7e9b7aedf0e054667de055b65af4a58e
Olle Kjellqvist <olle.kjellqvist@control.lth.se>
2021-02-11 21:57:25 +0100

    ...

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,4 @@
 include("../src/MinimaxAdaptiveControl.jl")
-using .MinimaxAdaptiveControl, Documenter
+using MinimaxAdaptiveControl, Documenter
 makedocs(sitename="Minimax Adaptive Control", modules=[MinimaxAdaptiveControl])
 deploydocs(repo = "github.com/kjellqvist/MinimaxAdaptiveControl.jl.git")
